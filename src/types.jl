674d9bcefb88399187caa00d597cf80299aed8fc
Anthony Clays <anthonyclays@gmail.com>
2014-12-25 20:33:39 +0100

    added rn_str macro, removed more weak type conversions, added hash method

--- src/types.jl
+++ src/types.jl
@@ -1,5 +1,3 @@
-import Base: showerror, convert, print, show, length, string
-
 # Thrown when the string passed to `parseroman` is not a valid Roman numeral.
 type InvalidRomanError <: Exception
     str::ASCIIString
@@ -10,19 +8,29 @@ Base.showerror(io::IO, err::InvalidRomanError) =
 immutable RomanNumeral <: Integer
     val::Int
     str::ASCIIString
+
     RomanNumeral(int::Integer) = new(int, toroman(int))
     RomanNumeral(str::ASCIIString) = begin
         num = parseroman(str)
         new(num, toroman(num))
     end
 end
+# This macro allows the rn"MMXV" syntactic sugar
+macro rn_str(str)
+    RomanNumeral(str)
+end
 
 typealias RN RomanNumeral
 
 # Standard functions
-Base.convert(::Type{Int}, num::RN) = num.val
+# Conversion + promotion
+#Base.convert{T<:Integer}(::Type{T}, num::RN) = convert(T, num.val)
+#Base.promote_rule{T<:Integer}(::Type{RN}, ::Type{T}) = RN
+Base.string(num::RN) = num.str
+
+# IO
 Base.print(io::IO, num::RN) = print(io, num.str)
 Base.show(io::IO, num::RN) = write(io, num.str)
+
 Base.length(num::RN) = length(num.str)
-Base.string(num::RN) = num.str
-#Base.int(num::RN) = num.val
+Base.hash(num::RN) = hash(num.str) $ hash(num.val)
