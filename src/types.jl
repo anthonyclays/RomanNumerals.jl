a9df573e888f11743260675b9b8dab16338296de
Sambit Kumar Dash <sambitdash@gmail.com>
2017-08-12 11:42:08 +0530

    Squashed commit of the following:
    commit 3884235e1f461ed9ae8d4d8a0a5f8a827f2a2fe2
    Merge: 7bead39 b7164d1
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Sat Aug 12 04:57:27 2017 +0530
    
        Merge branch 'master' of https://github.com/sambitdash/RomanNumerals.jl
    
    commit 7bead39a3de10f4ee8f19ef65d09abf7f446d1db
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 20:27:43 2017 +0530
    
        Revert "Type fixes for 0.7."
    
        This reverts commit bac9cf145a957375ddd8ac58a0678584f944b4e6.
    
    commit 4d4c3a255baef4912b8ab218cf410eb4762da93e
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:54:52 2017 +0530
    
        Ensuring minimal changes needed.
    
    commit 705f97767c2b93fa2e9ffb2d615d7ba5e82e950e
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:36:24 2017 +0530
    
        trying to fix for 0.5
    
    commit b7164d1455b1c57a068ae5313c266c6aa24d1aea
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 20:45:24 2017 +0530
    
        Revert "Type fixes for 0.7."
    
        This reverts commit 362857fd8d728d5254e1b78701016ab2809faaae.
    
    commit 21d8b061e5cfeca5a99e92046d611c8f846109e7
    Merge: a0794d3 362857f
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 20:33:28 2017 +0530
    
        Merge https://github.com/anthonyclays/RomanNumerals.jl
    
    commit a0794d3de7f318207375a9aee2b99abb3204300e
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 20:27:43 2017 +0530
    
        Revert "Type fixes for 0.7."
    
        This reverts commit bac9cf145a957375ddd8ac58a0678584f944b4e6.
    
    commit e7368044e3223e5fe3769f6297453fcda078ee7c
    Merge: bac9cf1 6419408
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 16:50:54 2017 +0530
    
        Merge branch 'master' of https://github.com/sambitdash/RomanNumerals.jl
    
    commit bac9cf145a957375ddd8ac58a0678584f944b4e6
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 16:44:07 2017 +0530
    
        Type fixes for 0.7.
    
    commit 64194089599f01d23ab4da91712b06fd0412f67d
    Merge: aad3c4d 596e4b3
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 16:28:15 2017 +0530
    
        Merge https://github.com/anthonyclays/RomanNumerals.jl
    
    commit aad3c4dd5d9b647459c01f7f81331458b71e454d
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:54:52 2017 +0530
    
        Ensuring minimal changes needed.
    
    commit f97a69504697e469fd8f75af29e55818194b7f1e
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:36:24 2017 +0530
    
        trying to fix for 0.5
    
    commit 17da6d6e82e14e2d0ed3e36d4366d92e0184107a
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:13:37 2017 +0530
    
        Removed 0.4 from travis-ci
    
    commit d5592b8023b261ac5a88f7217911c970caf05c51
    Author: Sambit Kumar Dash <sambitdash@gmail.com>
    Date:   Thu Aug 10 15:05:56 2017 +0530
    
        Fixing the errors for v0.6.

--- src/types.jl
+++ src/types.jl
@@ -1,13 +1,13 @@
 using Compat
 
 # Thrown when the string passed to `parseroman` is not a valid Roman numeral.
-@compat mutable struct InvalidRomanError <: Exception
+type InvalidRomanError <: Exception
     str::String
 end
 Base.showerror(io::IO, err::InvalidRomanError) =
     print(io, err.str, " is not a valid Roman numeral")
 
-@compat struct RomanNumeral <: Integer
+immutable RomanNumeral <: Integer
     val::Int
     str::String
 
@@ -28,9 +28,9 @@ const RN = RomanNumeral
 # Conversion + promotion
 Base.convert(::Type{Bool}, num::RN) = true
 Base.convert(::Type{BigInt}, num::RN) = BigInt(num.val)
-@compat Base.convert(::Type{T}, num::RN) where T <: Integer = convert(T, num.val)
+Base.convert{T<:Integer}(::Type{T}, num::RN) = convert(T, num.val)
 Base.convert(::Type{RN}, num::Int) = RN(num)
-@compat Base.promote_rule(::Type{RN}, ::Type{T}) where T <: Integer = T
+Base.promote_rule{T<:Integer}(::Type{RN}, ::Type{T}) = T
 Base.string(num::RN) = num.str
 
 # IO
