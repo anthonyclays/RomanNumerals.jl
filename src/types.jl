2fe956bf86772daf6d884187d8b121e6a70ef2c2
Giovanni Papini <gpapo95@gmail.com>
2018-06-11 19:18:08 +0200

    Replace conversions with constructors
    see:
    https://docs.julialang.org/en/latest/manual/conversion-and-promotion/#Conversion-vs.-Construction-1

--- src/types.jl
+++ src/types.jl
@@ -19,17 +19,13 @@ const RN = RomanNumeral
 
 # Standard functions
 # Conversion + promotion
-Base.convert(::Type{Bool}, num::RN) = true
-Base.convert(::Type{BigInt}, num::RN) = BigInt(num.val)
-Base.convert{T<:Integer}(::Type{T}, num::RN) = convert(T, num.val)
-Base.convert(::Type{RN}, num::Int) = RN(num)
-Base.promote_rule{T<:Integer}(::Type{RN}, ::Type{T}) = T
-Base.string(num::RN) = num.str
-Base.parse(::Type{RomanNumeral}, str::String) = RomanNumeral(str)
+Base.Bool(::RN) = true
+(::Type{T})(num::RN) where {T <: Union{Base.BitInteger,BigInt}} = T(num.val)
+
+Base.promote_rule(::Type{RN}, ::Type{T}) where {T <: Integer} = T
+# Base.string(num::RN) = num.str
 
 # IO
-Base.print(io::IO, num::RN) = print(io, num.str)
 Base.show(io::IO, num::RN) = write(io, num.str)
-
 Base.length(num::RN) = length(num.str)
 Base.hash(num::RN) = @compat xor(hash(num.str), hash(num.val))
